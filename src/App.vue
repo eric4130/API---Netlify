d77097d7986b8ff71ddf6f40f5905d463da6e37f
eric4130 <synthpad413@gmail.com>
2021-12-17 10:46:51 -0500

    update 2

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,9 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+  <router-view />
   </div>
-</template>
+  </template>
 
 <style lang="scss">
 #app {
